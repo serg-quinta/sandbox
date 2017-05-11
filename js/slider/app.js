b98ecd0179965e5b1d5923257d32b5cb38f5b07a
Serhiy Valchuk <svalchuk@gmail.com>
2017-05-11 19:28:21 +0300

    Updated slider

--- js/slider/app.js
+++ js/slider/app.js
@@ -1,37 +1,66 @@
 'use strict';
 class Slider {
-  constructor(selector) {
-    this.slider = document.querySelector(selector);
+  constructor(options) {
+    this.slider = document.querySelector(options.selector);
     this.thumb = this.slider.querySelector('[data-slider-thumb]');
 
     this.slider.addEventListener('mousedown', this._slide.bind(this));
+    this.pixelsPerValue = (this.slider.clientWidth - this.thumb.offsetWidth) / options.max;
   }
 
   _slide(event) {
     const target = event.target.closest('[data-slider-thumb]');
     if (!target) return;
 
-    document.addEventListener('mousemove', this._move.bind(this));
-    this.slider.addEventListener('mouseup', this._stop.bind(this));
+    this.moveHandler = this._move.bind(this);
+    document.addEventListener('mousemove', this.moveHandler);
+    this.stopHandler = this._stop.bind(this);
+    document.addEventListener('mouseup', this.stopHandler);
+
   }
 
   _move(event) {
-    let leftPosition = event.pageX - this.thumb.offsetWidth - this.slider.getBoundingClientRect().left / 2;
-    let leftMin = this.slider.getBoundingClientRect().left - this.thumb.offsetWidth / 2;
-    let leftMax = this.slider.getBoundingClientRect().right + this.thumb.offsetWidth / 2;
-    if (leftPosition > leftMax) {
-      this.thumb.style.left = this.slider.offsetWidth - this.thumb.offsetWidth / 2 + 'px';
-    } else if (leftPosition < leftMin) {
-      this.thumb.style.left = -this.thumb.offsetWidth / 2 + 'px';
+    let leftPosition = event.pageX - this.slider.getBoundingClientRect().left - this.thumb.offsetWidth / 2;
+    let leftMax = this.slider.offsetWidth - this.thumb.offsetWidth;
+    if (leftPosition < 0) {
+      this.thumb.style.left = 0 + 'px';
+    } else if (leftPosition > leftMax) {
+      this.thumb.style.left = leftMax + 'px';
     } else {
       this.thumb.style.left = leftPosition + 'px';
     }
-    console.log(leftPosition);
+    this._slideEvent();
   }
 
   _stop() {
-    document.removeEventListener('mousemove', this._move);
-    // this.slider.removeEventListener('mouseup', this._stop.bind(this));
-    console.log('Works!');
+    document.removeEventListener('mousemove', this.moveHandler);
+    document.removeEventListener('mouseup', this.stopHandler);
+    this._changeEvent();
+  }
+
+  _slideEvent() {
+    let slideEvent = new CustomEvent("slide", {
+      detail: document.querySelector('[data-slider-slide]').innerHTML = this._positionToValue(parseInt(this.thumb.style.left))
+    });
+    this.slider.dispatchEvent(slideEvent);
+  }
+
+  _changeEvent() {
+    let changeEvent = new CustomEvent("slide", {
+      detail: document.querySelector('[data-slider-change]').innerHTML = this._positionToValue(parseInt(this.thumb.style.left))
+    });
+    this.slider.dispatchEvent(changeEvent);
+  }
+
+  _positionToValue(left) {
+    return Math.round(left / this.pixelsPerValue);
+  }
+
+  _valueToPosition(value) {
+    return this.pixelsPerValue * value;
+  }
+
+  setValue(value) {
+    this.thumb.style.left = this._valueToPosition(value) + 'px';
   }
 }
